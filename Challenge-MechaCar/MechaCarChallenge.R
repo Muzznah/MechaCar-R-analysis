0bc6b2b4b79623c890f9de2c9043a211e1d37067
Muzznah Ansari <muzznahansari@gmail.com>
2020-07-31 21:42:53 -0400

    update script

--- Challenge-MechaCar/MechaCarChallenge.R
+++ Challenge-MechaCar/MechaCarChallenge.R
@@ -1,16 +1,16 @@
+# Load dependencies.
 library(tidyverse)
 
 #### MPG Regression ####
 
-
-#Load the MechaCar_mpg dataset.
+# Load the MechaCar_mpg dataset.
 ds <- 'MechaCar_mpg.csv'
 mecha_data <- read.csv(ds)
 
-#Check the first few rows of the dataset.
+# Check the first few rows of the dataset.
 head(mecha_data)
 
-#Do a quick qualitative check for a normal distribution.
+# Do a quick qualitative check for a normal distribution.
 ggplot(mecha_data, aes(x=mpg)) + geom_density()
 ggplot(mecha_data, aes(x=vehicle.length)) + geom_density()
 ggplot(mecha_data, aes(x=vehicle.weight)) + geom_density()
@@ -21,46 +21,46 @@ ggplot(mecha_data, aes(x=AWD)) + geom_density()
 # Perform Multiple Linear Regression Analysis
 # Use vehicle.length, vehicle.weight, spoiler.angle, ground.clearance, and AWD
 MLR <- lm(mpg ~ vehicle.length + vehicle.weight + spoiler.angle + ground.clearance, data = mecha_data)
- 
-#Obtain our statistical metrics using the summary() function.
+
+# Calculate summary statistics with summary() function.
 summary(MLR)
- 
+
 # Perform  2nd Multiple Linear Regression Analysis
 MLR1 <- lm(mpg ~ vehicle.length + ground.clearance , data = mecha_data)
 
-#Obtain our statistical metrics using the summary() function.
+# Calculate summary statistics with summary() function.
 summary(MLR1)
 
 #### Suspension Coil Summary ####
 
-#Load the suspension coil test data.
+# Load the suspension coil test data.
 coil_ds <- 'Suspension_Coil.csv'
 s_coil <- read.csv(coil_ds)
 
-#Do a quick qualitative check for a normal distribution.
+# Do a quick qualitative check for a normal distribution.
 ggplot(s_coil, aes(x=PSI)) + geom_density()
 
-#Check the first few rows of the dataset.
+# Check the first few rows of the dataset.
 head(s_coil)
 
-#Calculate, mean, median, standard deviation and variance
+# Calculate, mean, median, standard deviation and variance
 summary_coil <- s_coil%>% summarise(Mean=mean(PSI),Median=median(PSI),SD=sd(PSI),Variance=var(PSI))
 
-#Print the summary_coil table.
+# Print the summary_coil table.
 print(summary_coil)
 
 
-#Calculate, mean, median, standard deviation and variance by Lot type.
+# Calculate, mean, median, standard deviation and variance by Lot type.
 summary_byLot <- s_coil%>% group_by(Manufacturing_Lot) %>% summarise(Mean=mean(PSI),Median=median(PSI),SD=sd(PSI),Variance=var(PSI))
 
-#Print the summary_byLot table.
+# Print the summary_byLot table.
 print(summary_byLot)
 
 #### T-Test #####
 
-#Perform a two tailed t test with mu=1500.
+# Perform a two tailed t test with mu=1500.
 t_result<- t.test(s_coil$PSI,mu=1500)
 
 
-#Print t.test result.
+# Print t.test result.
 print(t_result)
